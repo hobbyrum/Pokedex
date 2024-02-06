a2bbd3af00c3f0e6a5acfb097d235c01a86f58f4
hobbyrum <9680372+hobbyrum@users.noreply.github.com>
2024-02-06 15:55:19 +0100

    🙂
    🙂🙂

--- Pokedex.ps1
+++ Pokedex.ps1
@@ -1,3 +1,4 @@
+$CSVData = "$($PSScriptRoot)\data.csv"
 class Pokemon {
     [Int]$DexNumber
     [String]$Name
@@ -37,7 +38,7 @@ class Pokemon {
         return "$($this.FinalEvolution)"
     }
 }
-$CSVData = "$($PSScriptRoot)\data.csv"
+
 $PokeDex = Import-CSV -Path $CSVData | ForEach-Object {
     [Pokemon]@{
         Name = $_.name
@@ -51,10 +52,48 @@ $PokeDex = Import-CSV -Path $CSVData | ForEach-Object {
     }
 }
 
-function SearchForPokemon {
-    param ($PokemonToSearchFor)
+$PokemonTypeColors = @{
+    Grass = "Green"
+    Poison = "DarkGreen"
+    Fire = "Red"
+    Flying = "Gray"
+    Water = "Blue"
+    Bug = "Green"
+    Normal = "White"
+    Electric = "Yellow"
+    Fairy = "Cyan"
+    Ground = "DarkGray"
+    Fighting = "DarkRed"
+    Psychic = "Magenta"
+    Rock = "DarkYellow"
+    Ice = "Blue"
+    Ghost = "Gray"
+    Steel = "Blue"
+}
 
-    [String]$PokemonToSearchFor = Read-Host "Search for Pokemon by name"
+$PokemonTypes = @(
+    "Grass"
+    "Poison"
+    "Fire"
+    "Flying"
+    "Water"
+    "Bug"
+    "Normal"
+    "Electric"
+    "Fairy"
+    "Ground"
+    "Fighting"
+    "Psychic"
+    "Rock"
+    "Ice"
+    "Ghost"
+    "Steel"
+)
+
+function SearchForPokemon {
+    param ([String]$PokemonToSearchFor)
+
+    $PokemonToSearchFor = Read-Host "Search for Pokemon by name"
 
     $SearchedForPokemon = $PokeDex | Where-Object { $_.Name -like "*$PokemonToSearchFor*" } | Select-Object -ExpandProperty Name
 
@@ -63,49 +102,118 @@ function SearchForPokemon {
     } else {
         Write-Host "No matches found for: $PokemonToSearchFor"
     }
-    MainMenu $false
+    Pause
 }
-function DisplayPokemon {
-    [String]$PokemonToDisplay = Read-Host "Which pokeman do you want to see information about: "
+
+function ShowRandomPokemon {
+    $RandomPokemon = $PokeDex | Get-Random | Select-Object -ExpandProperty Name
+
+    Write-Host "Random pokeman is:`n"
+    ShowPokemon $RandomPokemon
+}
+
+function ShowPokemonByType {
+    param([String]$PokemonByType)
+
+    if (-not $PokemonByType) {
+        $PokemonByType = Read-Host "Enter a pokemon type"
+    }
+
+    $AllPokemonByType = $Pokedex | Where-Object { $_.Type -eq $PokemonByType } | Select-Object -ExpandProperty Name
+    
+    foreach ($Type in $PokemonTypes) {
+        switch ($Type) {
+            "Grass" {
+                Write-Host "All $PokemonByType pokemen: $($AllPokemonByType -join ', ')"
+            }
+        }
+    }
+    # Main
+}
+ 
+function ShowPokemon {
+    param ([String]$PokemonToDisplay)
+
+    if (-not $PokemonToDisplay) {
+        $PokemonToDisplay = Read-Host "Which pokeman do you want to see information about: "
+    }
 
     $SelectedPokemon = $PokeDex | Where-Object { $_.Name -eq $PokemonToDisplay }
+    $SelectedName = $SelectedPokemon.GetName()
+    $SelectedType = $SelectedPokemon.GetType() -Split ' and '
+    $SelectedGender = $SelectedPokemon.GetGender()
     $EvolvedFrom = $Pokedex | Where-Object { $_.DexNumber -eq $SelectedPokemon.EvolvedFrom } | Select-Object -ExpandProperty Name
+    $SelectedHeight = $SelectedPokemon.GetHeight()
+    $SelectedWeight = $SelectedPokemon.GetWeight()
+    $SelectedDexNumber = $SelectedPokemon.GetDexNumber()
 
     if ($SelectedPokemon) {
-        Write-Host "Name: $($SelectedPokemon.GetName())"
-        Write-Host "Type: $($SelectedPokemon.GetType())"
-        Write-Host "Gender: $($SelectedPokemon.GetGender())"
+        Write-Host $SelectedName
+
+        # $PokemonTypes = $SelectedPokemon.GetType() -split ' and '
+
+        foreach ($Type in $SelectedType) {
+            $TypeColor = $PokemonTypeColors[$Type]
+            Write-Host $Type -ForegroundColor $TypeColor
+        }
+        
+        Write-Host "Gender: $($SelectedGender)"
         If ($EvolvedFrom) { Write-Host "Evolved from: $($EvolvedFrom)" }
-        Write-Host "Height: $($SelectedPokemon.GetHeight()) in"
-        Write-Host "Weight: $($SelectedPokemon.GetWeight()) lbs"
-        Write-Host "Dex no.: $($SelectedPokemon.GetDexNumber())"
-        If ($SelectedPokemon.FinalEvolution -eq 'true' -and $SelectedPokemon.EvolvedFrom -ne 0) { Write-Host "Final evolution" }
+
+        Write-Host "Height: $($SelectedHeight) in"
+        Write-Host "Weight: $($SelectedWeight) lbs"
+        Write-Host "Pokeman no.: $($SelectedDexNumber)`n"
     } else {
         Write-Host "Pokeman `'$($PokemonToDisplay)`' not found"
     }
-    MainMenu $false
+    Pause
+    Main
 }
 
 function MainMenu {
-    param ([Boolean]$FirstVisit = $true)
-        if ($FirstVisit) {
-            Write-Host "`nWelcome to the magical Pokedex"
-        }
-
-        $NewLine = If ($FirstVisit) { "`n" } else { "" }
-        $WhatToDo = Read-Host -Prompt ($NewLine + "`n1: Search for pokemon by name`n2: Display information about a specific pokemon`nEnter your choice:")
-
-        switch($WhatToDo) {
-            1 { SearchForPokemon; break }
-            2 { DisplayPokemon; break }
-            default { Write-Host "Invalid option. Enter a valid choice."}
-        }
+    Clear-Host
+    Write-Host "======== Pokedex ==========`n"
+    Write-Host "1. Search for pokeman by name"
+    Write-Host "2. Display information about a specific pokeman"
+    Write-Host "3. Show information about a random pokeman"
+    Write-Host "4. Show all pokemen by type"
+    Write-Host "5. Exit`n"
 }
-MainMenu
 
-# function DisplayAllPokemon {
-#     foreach ($pokemon in $Pokedex) {
-#         Write-Host "Name: $($pokemon.GetName()) | Type: $($pokemon.GetType()) | Evolved from: $($pokemon.GetEvolvedFrom())"
-#     }
-# }
-# DisplayAllPokemon
+function ExitApplication {
+    $Title = "Good evening"
+    $Question = "Exit the program?"
+    $Choices = @(
+        [System.Management.Automation.Host.ChoiceDescription]::new("&Yes", "What is the charge?!")
+        [System.Management.Automation.Host.ChoiceDescription]::new("&No", "Are you the one...?")
+        )
+    $DefaultChoice = -1
+
+    $Decision = $Host.UI.PromptForChoice($Title, $Question, $Choices, $DefaultChoice)
+
+    if ($Decision -eq 0) {
+        Write-Host "Tata and farewell..."
+        Exit
+    } else {
+        Main
+    }
+}
+
+function Main {
+    do {
+        MainMenu
+        $Choice = Read-Host "What do you want to do? Enter a number"
+
+        switch ($Choice) {
+            1 { SearchForPokemon }
+            2 { ShowPokemon }
+            3 { ShowRandomPokemon }
+            4 { ShowPokemonByType }
+            5 { ExitApplication }
+            default { Write-Host "Invalid choice" }
+        }
+    } while ($Choice -ne 4)
+}
+
+# Main
+ShowPokemonByType -PokemonByType "Grass"
